0990d67170d46295c686790ae394e67de92614ff
arafatkh999 <arafatkh999@gmail.com>
2022-02-07 13:22:26 +0600

    mail setting done

--- lib/view/about.dart
+++ lib/view/about.dart
@@ -23,45 +23,51 @@ class _AboutUSState extends State<AboutUS> {
           child: Card(
             elevation: 15,
             child: Column(
+
               children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage('images/profile.jpg'),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('images/profile.jpg'),
+
+                  ),
                 ),
                 Text("Khondokar Arafat"),
                 Text('Flutter App Developer'),
                 Text('ID-A3-1883'),
                 Divider(height: 8,),
                 Center(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
-                        final url='https://www.facebook.com/arafatkh999';
-                        if(await canLaunch(url)){
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
+                          final url='https://www.facebook.com/arafatkh999';
+                          if(await canLaunch(url)){
+                            await launch(url);
+                          }
+
+                        },),
+                        InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
+                          final url='https://github.com/arafatkh999';
+                          if(await canLaunch(url)){
                           await launch(url);
-                        }
+                          }
+                        },),
+                        InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
+                          final url='https://www.linkedin.com/in/arafatkh999';
+                          if(await canLaunch(url)){
+                          await launch(url);
+                          }
+                        },),
+                        InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: (){
+                          launch("mailto:arafatkh999@gmail.com");
 
-                      },),
-                      InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
-                        final url='https://github.com/arafatkh999';
-                        if(await canLaunch(url)){
-                        await launch(url);
-                        }
-                      },),
-                      InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
-                        final url='https://www.linkedin.com/in/arafatkh999';
-                        if(await canLaunch(url)){
-                        await launch(url);
-                        }
-                      },),
-                      InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: ()async{
-                        final url='https://www.arafatkh999@gmail.com';
-                        if(await canLaunch(url)){
-                        await launch(url);
-                        }
-                      },),
+                        },),
 
-                    ],
+                      ],
+                    ),
                   ),
                 )
 
@@ -75,44 +81,48 @@ class _AboutUSState extends State<AboutUS> {
               elevation: 15,
               child: Column(
                 children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/labib.jpg'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('images/labib.jpg'),
+                    ),
                   ),
                   Text("Labib Ashab"),
                   Text('Flutter App Developer'),
                   Text('ID-A3-1891'),
                   Divider(height: 8,),
                   Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
-                          final url='https://www.facebook.com/ashablabib';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
+                            final url='https://www.facebook.com/ashablabib';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
 
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
-                          final url='http://github.com/labibashab';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
-                          final url='https://www.linkedin.com/in/labib-ashab-530965230';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: ()async{
-                          final url='https://www.mdlabibashab1@gmail.com';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
+                            final url='http://github.com/labibashab';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
+                            final url='https://www.linkedin.com/in/labib-ashab-530965230';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: (){
+                            launch("mailto:mdlabibashab1@gmail.com");
 
-                      ],
+                          },),
+
+                        ],
+                      ),
                     ),
                   )
 
@@ -126,44 +136,48 @@ class _AboutUSState extends State<AboutUS> {
               elevation: 15,
               child: Column(
                 children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/niloy.jpg'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('images/niloy.jpg'),
+                    ),
                   ),
                   Text("Niloy Biswas"),
                   Text('Flutter App Developer'),
                   Text('ID-A3-1883'),
                   Divider(height: 8,),
                   Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
-                          final url='https://www.facebook.com/niloy.biswas.184881';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
+                            final url='https://www.facebook.com/niloy.biswas.184881';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
 
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
-                          final url='http://github.com/biswasniloy';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
-                          final url='https://www.linkedin.com/feed/?trk=cold_join_sign_in';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: ()async{
-                          final url='https://www.arafatkh999@gmail.com';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
+                            final url='http://github.com/biswasniloy';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
+                            final url='https://www.linkedin.com/feed/?trk=cold_join_sign_in';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: (){
+                            launch("mailto:niloybiswas917@gmail.com");
 
-                      ],
+                          },),
+
+                        ],
+                      ),
                     ),
                   )
 
@@ -177,44 +191,47 @@ class _AboutUSState extends State<AboutUS> {
               elevation: 15,
               child: Column(
                 children: [
-                  CircleAvatar(
-                    backgroundImage: AssetImage('images/ap.jpg'),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage('images/ap.jpg'),
+                    ),
                   ),
                   Text("Apurba Kundu"),
                   Text('Flutter App Developer'),
                   Text('ID-A3-1884'),
                   Divider(height: 8,),
                   Center(
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
-                          final url='https://www.facebook.com/apurba.mankind';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          InkWell(child: FaIcon(FontAwesomeIcons.facebook),onTap: ()async{
+                            final url='https://www.facebook.com/apurba.mankind';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
 
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
-                          final url='https://github.com/apurba888';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
-                          final url='https://www.linkedin.com/in/apurba-kundu-193896230';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
-                        InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: ()async{
-                          final url='https://www.arafatkh999@gmail.com';
-                          if(await canLaunch(url)){
-                            await launch(url);
-                          }
-                        },),
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.github),onTap: ()async{
+                            final url='https://github.com/apurba888';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.linkedin),onTap: ()async{
+                            final url='https://www.linkedin.com/in/apurba-kundu-193896230';
+                            if(await canLaunch(url)){
+                              await launch(url);
+                            }
+                          },),
+                          InkWell(child: FaIcon(FontAwesomeIcons.envelope),onTap: (){
+                            launch("mailto:nod.goodboy@gmail.com");
+                          },),
 
-                      ],
+                        ],
+                      ),
                     ),
                   )
 
